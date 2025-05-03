1e08ace82ed28890d611aa5025cfdbc532404928
georgejames123 <georgejames123@yahoo.com>
2025-05-03 20:21:57 +0800

    init commit

--- springapi/src/main/java/com/test/springapi/APIController.java
+++ springapi/src/main/java/com/test/springapi/APIController.java
@@ -3,12 +3,17 @@ package com.test.springapi;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLOutput;
+import java.util.Date;
+
 @RestController
 @RequestMapping("/api")
 public class APIController {
 
     @GetMapping("/hello")
     public String hello() {
+        System.out.println("Hello from Spring Boot!");
+        System.out.println(new Date().toString());
         return "Hello from Spring Boot!";
     }
 }
